e18e9f629867ef7dee3554f3483dfb4e076e2803
maxjeanty <maxjeanty@gmail.com>
2023-12-05 17:54:57 -0600

    added draggability funtions can drag/drop items

--- script.js
+++ script.js
@@ -51,19 +51,34 @@ function createList(){
 
 
 function dragStart(){
+   dragStartIndex = +this.closest('li').getAttribute('data-index')
    
+  
 }
 function dragEnter(){
-   
+    this.classList.add('over')
 }
-function dragOver(){
+function dragOver(e){
+    e.preventDefault()
     
 }
 function dragLeave(){
-  
+    this.classList.remove('over')
+
 }
 function dragDrop(){
+    const dragEndIndex = +this.getAttribute('data-index')
+    swapItems(dragStartIndex, dragEndIndex)
+
+    this.classList.remove('over')
+}
+
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable')
+    const itemTwo = listItems[toIndex].querySelector('.draggable')
     
+    listItems[fromIndex].appendChild(itemTwo)
+    listItems[toIndex].appendChild(itemOne)
 }
 
 function addEventListeners(){
